8676fb4356f60d8ccc578adffb57dc5eaddb7a7e
neskinggoblin <fnik@gmail.com>
2025-03-27 17:54:40 +0700

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envPrefix: 'VITE_',
- 
+  server: {
+    proxy: {
+      '/products': {
+        target: 'https://readhaven-p7oc.onrender.com',  // ตั้งค่า URL ของ backend
+        changeOrigin: true,  // เปลี่ยน origin ของคำขอให้ตรงกับ target
+        secure: false,  // ตั้งเป็น false ถ้าไม่ใช้ HTTPS
+      
+      },
+    },
+  },
 })
