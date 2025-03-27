150a4ebdf732b22901d22f195a52b0b11f44763a
neskinggoblin <fnik@gmail.com>
2025-03-27 20:23:46 +0700

    leafmapapi config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envPrefix: 'VITE_',
-  server: {
-    proxy: {
-      '/products': {
-        target: 'https://readhaven-p7oc.onrender.com',  // ตั้งค่า URL ของ backend
-        changeOrigin: true,  // เปลี่ยน origin ของคำขอให้ตรงกับ target
-        secure: false,  // ตั้งเป็น false ถ้าไม่ใช้ HTTPS
-      
-      },
-    },
-  },
+ 
 })
